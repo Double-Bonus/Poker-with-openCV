f0c70de0915a2aba2760e6bcabc28f814fc0399c
Double-Bonus <zygimantasmarma@gmail.com>
2021-04-15 00:06:01 +0300

    Changed to gray

--- FirstTry/FirstTry.cpp
+++ FirstTry/FirstTry.cpp
@@ -1,75 +1,77 @@
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include <iostream>
-#include <string>
-#include <sstream>
-
-
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
 using namespace std;
 using namespace cv;
-
-int match(string filename, string templatename);
-
-int main(int argc, char** agrv)
+bool use_mask;
+Mat img; Mat templ; Mat mask; Mat result;
+const char* image_window = "Source Image";
+const char* result_window = "Result window";
+int match_method;
+int max_Trackbar = 5;
+void MatchingMethod(int, void*);
+int main(int argc, char** argv)
 {
-    int test  = match("img_poker", "template");
-    return 0;
-}
-
-
-
-int match(string filename, string templatename)
-{
-    Mat ref = cv::imread(filename + ".png");
-    Mat tpl = cv::imread(templatename + ".png");
-    if (ref.empty() || tpl.empty())
+    if (argc < 3)
     {
-        cout << "Error reading file(s)!" << endl;
+        cout << "Not enough parameters" << endl;
+        cout << "Usage:\n" << argv[0] << " <image_name> <template_name> [<mask_name>]" << endl;
         return -1;
     }
+    img = imread(argv[1], IMREAD_COLOR);
+    templ = imread(argv[2], IMREAD_COLOR);
+    cvtColor(img, img, COLOR_BGR2GRAY);
+    cvtColor(templ, templ, COLOR_BGR2GRAY);
 
-    Mat gref, gtpl;
-    cvtColor(ref, gref, COLOR_BGRA2GRAY);
-    cvtColor(tpl, gtpl, COLOR_BGRA2GRAY);
-
-    const int low_canny = 110;
-    Canny(gref, gref, low_canny, low_canny * 3);
-    Canny(gtpl, gtpl, low_canny, low_canny * 3);
-
-    imshow("file", gref);
-    imshow("template", gtpl);
-
-    Mat res_32f(ref.rows - tpl.rows + 1, ref.cols - tpl.cols + 1, CV_32FC1);
-    matchTemplate(gref, gtpl, res_32f, NORM_MINMAX);
-
-    Mat res;
-    res_32f.convertTo(res, CV_8U, 255.0);
-    imshow("result", res);
-
-    int size = ((tpl.cols + tpl.rows) / 4) * 2 + 1; //force size to be odd
-    adaptiveThreshold(res, res, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, size, -64);
-    imshow("result_thresh", res);
-
-    while (1)
-    {
-        double minval, maxval;
-        Point minloc, maxloc;
-        minMaxLoc(res, &minval, &maxval, &minloc, &maxloc);
-
-        if (maxval > 0)
-        {
-            rectangle(ref, maxloc, Point(maxloc.x + tpl.cols, maxloc.y + tpl.rows), Scalar(0, 255, 0), 2);
-            floodFill(res, maxloc, 0); //mark drawn blob
-        }
-        else
-            break;
+    if (argc > 3) {
+        use_mask = true;
+        mask = imread(argv[3], IMREAD_COLOR);
     }
-
-    imshow("final", ref);
+    if (img.empty() || templ.empty() || (use_mask && mask.empty()))
+    {
+        cout << "Can't read one of the images" << endl;
+        return EXIT_FAILURE;
+    }
+    namedWindow(image_window, WINDOW_AUTOSIZE);
+    namedWindow(result_window, WINDOW_AUTOSIZE);
+    const char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
+    createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod);
+    MatchingMethod(0, 0);
     waitKey(0);
-
-    return 0;
+    return EXIT_SUCCESS;
+}
+void MatchingMethod(int, void*)
+{
+    Mat img_display;
+    img.copyTo(img_display);
+    int result_cols = img.cols - templ.cols + 1;
+    int result_rows = img.rows - templ.rows + 1;
+    result.create(result_rows, result_cols, CV_32FC1);
+    bool method_accepts_mask = (TM_SQDIFF == match_method || match_method == TM_CCORR_NORMED);
+    if (use_mask && method_accepts_mask)
+    {
+        matchTemplate(img, templ, result, match_method, mask);
+    }
+    else
+    {
+        matchTemplate(img, templ, result, match_method);
+    }
+    normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+    double minVal; double maxVal; Point minLoc; Point maxLoc;
+    Point matchLoc;
+    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+    if (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED)
+    {
+        matchLoc = minLoc;
+    }
+    else
+    {
+        matchLoc = maxLoc;
+    }
+    rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+    rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+    imshow(image_window, img_display);
+    imshow(result_window, result);
+    return;
 }
